00ae4007e4f87e6a01205c9685a1b15e6ceee6f7
max <maxence.landa@gmail.com>
2023-12-09 18:27:25 +0100

    menu

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -6,13 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+
 class MenuController extends AbstractController
 {
+
+    private ProductRepository $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
     #[Route('/menu', name: 'app_menu')]
     public function index(): Response
     {
-        return $this->render('menu/index.html.twig', [
-            'controller_name' => 'MenuController',
+        return $this->render(
+            'menu/index.html.twig', [
+            'products' => $this->productRepository->findAll(),
         ]);
     }
 }
