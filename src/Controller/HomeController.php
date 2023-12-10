165c15fc097774ecc43e67fb278ca88157855dc0
Vladimir Boussekeyt <v.boussekeyt@ecoles-epsi.net>
2023-12-10 15:55:14 +0100

    Page a propos + modif lien controller + modif fichier css

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,10 +8,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route(path: '/', name: 'app_home')]
+    #[Route(path: '/', name: 'index')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [ #il retourne une reponse : le render
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route(path: '/accueil', name: 'accueil')]
+    public function accueil(): Response
+    {
+        return $this->render('home/index.html.twig', [ #il retourne une reponse : le render
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route(path: '/aPropos', name: 'aPropos')]
+    public function aPropos(): Response
+    {
+        return $this->render('home/aPropos.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
